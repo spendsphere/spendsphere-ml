852d30bf5fe8e36ab9a7102758e5f50a145806fa
d.barsukova <d.barsukova@g.nsu.ru>
2025-12-01 14:18:14 +0700

    add main.py

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import os  # noqa: F401
+
+from src.core.config import OLLAMA_API_URL  # noqa: F401
+
+if __name__ == "__main__":
+    prompt = "Extract text from this sample receipt."
+    print(prompt)
